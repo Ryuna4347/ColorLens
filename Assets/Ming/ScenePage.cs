1f3fba5d9c4771afda52f8fb89ec9b04c3715fda
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-09-04 09:22:53 +0900

    9_4_스테이지선택씬 페이지시스템
    일단 24스테이지까지 제작, 유동적으로 더 조절 가능

---
+++ Assets/Ming/ScenePage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenePage : MonoBehaviour
+{
+  public GameObject[] pages;
+  public int index = 0;
+  public GameObject leftBtn, rightBtn;
+  private void Update()
+  {
+    for (int i = 0; i < pages.Length; i++)
+    {
+      if (i == index)
+        pages[i].SetActive(true);
+      else
+        pages[i].SetActive(false);
+    }
+
+    if (index == 0)
+    {
+      leftBtn.SetActive(false);
+      rightBtn.SetActive(true);
+    }
+
+    if (index == pages.Length-1)
+    {
+      rightBtn.SetActive(false);
+      leftBtn.SetActive(true);
+    }
+  }
+
+  public void left()
+  {
+    index--;
+  }
+
+  public void right()
+  {
+    index++;
+  }
+}
