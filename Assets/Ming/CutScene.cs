f4602c2e90a5b1680a3e6e31b58e14d926510e48
Ryuna <yhw7979@naver.com>
2020-10-04 01:41:51 +0900

    Edit : Cutscene & In-Game Tutorial UI
    1. 1-1 스테이지 인게임 튜토리얼 UI 추가
    2. 인게임 튜토리얼이 컷신을 가리지 않도록 수정
    3. 컷신 마지막 장면 이후 클릭하면 여백 배경도 같이 투명해진 후 튜토리얼이 나오도록 수정
    4. 게임매니저와의 호출 순서를 지키기 위해 컷신의 Awake->Start로 변경

--- Assets/Ming/CutScene.cs
+++ Assets/Ming/CutScene.cs
@@ -10,16 +10,19 @@ public class CutScene : MonoBehaviour
     public Image scene;
     public Sprite[] sceneImages;
 
-    private void Awake()
+    private void Start()
     {
-        if (SceneManager.GetActiveScene().name == "1-1" || SceneManager.GetActiveScene().name == "2-1" ||
-            SceneManager.GetActiveScene().name == "3-1" || SceneManager.GetActiveScene().name == "4-1")
+        if (SceneManager.GetActiveScene().name == "1-1")
         {
-           if (PlayerPrefs.GetInt(SceneManager.GetActiveScene().name,0) == 0)
-                 {
-                     PlayerPrefs.SetInt(SceneManager.GetActiveScene().name,1);
-                     StartCoroutine(cutSceneCor());
-                 }   
+            if (PlayerPrefs.GetInt(SceneManager.GetActiveScene().name, 0) == 0)
+            {
+                PlayerPrefs.SetInt(SceneManager.GetActiveScene().name, 1);
+                StartCoroutine(cutSceneCor());
+            }
+            else //한 번 컷신을 본 경우 컷신을 더 이상 보여주지 않는다.
+            {
+                GameManager.instance.CheckTutorial();
+            }
         }
     }
 
@@ -32,20 +35,28 @@ public class CutScene : MonoBehaviour
             imgs[sceneImages.Length-1-i].sprite = sceneImages[i];
         }
         Color color=Color.white;
-        for (int i = sceneImages.Length-1; i >=0; i--)
+        for (int i = sceneImages.Length - 1; i >= 0; i--)
         {
-            yield return new WaitUntil(()=>Input.GetMouseButtonDown(0));
-            color=Color.white;
-            while (color.a>0)//무한반복
+            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+            color = Color.white;
+            while (color.a > 0)//무한반복
             {
                 color = imgs[i].color;
                 color.a -= 0.01f;
                 imgs[i].color = color;
+
+                if(i == 0) //맨 마지막 컷신은 주위에 있는 검은 배경도 같이 투명화되도록
+                {
+                    foreach(Transform background in imgs[i].gameObject.transform)
+                    {
+                        background.gameObject.GetComponent<Image>().color = new Color(0,0,0,color.a);
+                    }
+                }
+
                 yield return new WaitForSeconds(0.01f); //0.2초의 간격을 두고 실행됨  
             }
-
             imgs[i].gameObject.SetActive(false);
         }
-        //FindObjectOfType<Pause>().CanPuse = true;
+        GameManager.instance.CheckTutorial();
     }
 }
