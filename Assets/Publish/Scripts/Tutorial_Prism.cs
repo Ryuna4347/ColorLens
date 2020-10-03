4f25182d888d117c7d02f6c912b6943f8192d0e6
Ryuna <yhw7979@naver.com>
2020-10-03 19:12:39 +0900

    Edit : Change CutScene & Turotial UI
    1. 컷신 이미지 수정
    2. 튜토리얼 UI에서 프리즘 통과시 프리즘 이펙트가 실행되도록 수정

---
+++ Assets/Publish/Scripts/Tutorial_Prism.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_Prism : MonoBehaviour
+{
+    public List<GameObject> PrismObjs= new List<GameObject>();
+    
+    public void StartPrismEffect()
+    {
+        Debug.Log(gameObject.name);
+        foreach (GameObject prism in PrismObjs)
+        {
+            prism.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().Play();
+        }
+            
+    }
+}
