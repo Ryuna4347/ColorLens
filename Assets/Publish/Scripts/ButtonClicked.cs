945ae1b73f8943592d288bd8746ef394b19a1fc9
Ryuna <yhw7979@naver.com>
2020-10-04 18:52:44 +0900

    Edit : Modify
    1. 일부 맵 수정
    2. 타이틀 씬 도움말 버튼 클릭 사운드 추가
    3. 이미지 수정중

---
+++ Assets/Publish/Scripts/ButtonClicked.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClicked : MonoBehaviour
+{
+    public void PlaySound(string name)
+    {
+        SoundManager.instance.Play(name);
+    }
+}
