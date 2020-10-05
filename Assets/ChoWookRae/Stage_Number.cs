c6196a7bc595fb72d40a0565e53f6b217323a3a3
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2020-10-05 13:08:19 +0900

    일시정지 했을 때 스테이지 표시기능

---
+++ Assets/ChoWookRae/Stage_Number.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Stage_Number : MonoBehaviour
+{
+    public Text Stage_Text;
+    void Start()
+    {
+        Stage_Text.text = SceneManager.GetActiveScene().name;
+    }
+}
