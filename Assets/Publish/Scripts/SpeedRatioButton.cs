f47eb3cebf5f2a288e7a05dc582fe61c42aa5e02
Ryuna <yhw7979@naver.com>
2020-10-06 17:07:14 +0900

    1. 이미지 수정

--- Assets/Publish/Scripts/SpeedRatioButton.cs
+++ Assets/Publish/Scripts/SpeedRatioButton.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpeedRatioButton : MonoBehaviour
 {
     private bool buttonPressed = false;
+    private Image buttonImg;
+    public List<Sprite> ratioImages;
+
+    private void Awake()
+    {
+        buttonImg = GetComponent<Image>();
+    }
 
     public void ChangeMoveSpeedRatio()
     {
         buttonPressed = !buttonPressed;
+        buttonImg.sprite = ratioImages[buttonPressed?1:0];
         float speedRatio = buttonPressed ? 1.5f : 1.0f;
         GameManager.instance.ChangeMoveRatio(speedRatio);
     }
