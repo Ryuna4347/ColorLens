b754df98f2600faa958ff34c1789bdd3818c2c2e
Ryuna <yhw7979@naver.com>
2020-10-07 19:31:05 +0900

    Add : Final CutScene
    1. 뒤로가기 버튼 일부가 소리가 달라서 수정
    2. 4-12스테이지 클리어시 chap4 씬에서 나오는 컷신 추가
    3. 스테이지 시작 시 일시정지 UI가 켜진상태로 시작되는 오류 수정

---
+++ Assets/Publish/Scripts/FinalCutScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FinalCutScene : MonoBehaviour, IPointerClickHandler {
+    private void Start()
+    {
+        if(PlayerPrefs.GetInt("4-12",0)>0 && PlayerPrefs.GetInt("FinalCutScene",0)==0)
+        {
+            transform.localPosition = new Vector3(0, 0, 0);
+        }
+        else //아직 4-12를 클리어하지 못했거나 컷씬을 본 경우 이 캔버스는 필요가 없으므로 Off로 둔다.(클릭 이벤트가 있으므로 꺼두는게 문제가 안 생길 것)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PlayerPrefs.SetInt("FinalCutScene", 1);
+        gameObject.SetActive(false);
+    }
+}
