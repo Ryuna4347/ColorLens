d2a8076a401358e8ef7969be3235e306edc86469
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2020-10-28 15:37:50 +0900

    별이동 조건UI
    Stage_Number스크립트에서 작업

--- Assets/ChoWookRae/Stage_Number.cs
+++ Assets/ChoWookRae/Stage_Number.cs
@@ -6,8 +6,25 @@ using UnityEngine.UI;
 public class Stage_Number : MonoBehaviour
 {
     public Text Stage_Text;
+    public Text Star_requirement;
+    List<int> baseMoveCount;//각 맵마다 별을 획득할 수 있는 이동횟수 기준(2개)
+
+    public GameObject PausePanel;
+    public GameObject GameoverPanel;
+    public GameObject ClearPanel;
+    public GameObject G_Star_requirement;
     void Start()
     {
+        baseMoveCount = GameObject.Find("Map").GetComponent<Map>().GetBaseMoveCount();
         Stage_Text.text = SceneManager.GetActiveScene().name;
+    }
+
+    private void Update()
+    {
+        if (PausePanel.active == true || GameoverPanel.active == true || ClearPanel.active == true)
+            G_Star_requirement.SetActive(true);
+        else
+            G_Star_requirement.SetActive(false);
+        Star_requirement.text = "별 3개: " +baseMoveCount[1] +"\n별 2개: "+ baseMoveCount[0];
     }
 }
